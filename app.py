e2567f97a2c0ebd813f819794a959d77469c5f3d
prabhatsingh03 <prabhat.singh@adventz.com>
2025-09-10 09:56:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -694,5 +694,5 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all() # Create database tables if they don't exist
         run_startup_migrations()
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
 
